01bdf26ac7ca79793e963dd7f2c5dcb3ef067821
Katt Benny <kbenny9@icloud.com>
2019-10-16 19:29:53 -0400

    First pass at V2

--- Week03/CompletedSolution/MovieApplication/MainWindow.xaml.cs
+++ Week03/CompletedSolution/MovieApplication/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,13 @@ namespace MovieApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ObservableCollection<Movie> movieList = new ObservableCollection<Movie>();
+
         public MainWindow()
         {
             InitializeComponent();
+            movieListView.ItemsSource = movieList;
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -32,7 +37,11 @@ namespace MovieApplication
 
         private void submitMovieButton_Click(object sender, RoutedEventArgs e)
         {
-            Movie submittedMovie = new Movie(titleTextBox.Text, genreTextBox.Text, Double.Parse(reviewScoreTextBox.Text));
+            
+
+            Movie submittedMovie = new Movie(titleTextBox.Text, genreTextBox.Text, double.Parse(reviewScoreTextBox.Text));
+
+            movieList.Add(submittedMovie);
 
             //submittedMovie.Title = titleTextBox.Text;
             //submittedMovie.Genre = genreTextBox.Text;
@@ -40,6 +49,15 @@ namespace MovieApplication
             //submittedMovie.ReviewScore = Double.Parse(reviewScoreTextBox.Text);
 
             submittedMovie.DisplayInformation();
+
+            titleTextBox.Clear();
+            genreTextBox.Clear();
+            reviewScoreTextBox.Clear(); 
+
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
